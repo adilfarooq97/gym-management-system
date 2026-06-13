802987d5c5e2eb51f5672c331875630d95144074
Adil Farooq <adilfar3540@gmail.com>
2026-06-13 19:46:08 +0500

    checking commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,9 @@
 const express = require("express");
-
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Gym API Running");
+  res.send("Gym API is  Running");
 });
 
 app.listen(3000, () => {
